7c133691664d942f52fc6a4404c36f7fa26b8c2b
Maxime Preaux <hello@maxime.dev>
2022-06-07 15:11:22 -0700

    Added an identify call when user documents are created

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -15,15 +15,17 @@ function getSegment() {
   return _segment;
 }
 
-// // Start writing Firebase Functions
-// // https://firebase.google.com/docs/functions/typescript
-
+// https://firebase.google.com/docs/functions/typescript
 // https://firebase.google.com/docs/functions/auth-events
+// https://firebase.google.com/docs/functions/firestore-events
 
 // Note: trigger functions require a return value or promise
 
 const GROUP_ID = "opensourcehub" as const;
 
+/**
+ * Called when a new user signs up with Firebase auth
+ */
 export const userCreated = functions.auth.user().onCreate((user) => {
   functions.logger.info("User created", { email: user.email, uid: user.uid });
 
@@ -31,7 +33,7 @@ export const userCreated = functions.auth.user().onCreate((user) => {
     .identify({
       userId: user.uid,
       traits: {
-        email: user.email,
+        oshEmail: user.email,
       },
     })
     .group({
@@ -46,6 +48,9 @@ export const userCreated = functions.auth.user().onCreate((user) => {
   return Promise.resolve();
 });
 
+/**
+ * Called when a user deletes their account
+ */
 export const userDeleted = functions.auth.user().onDelete((user) => {
   functions.logger.info("User deleted", { email: user.email, uid: user.uid });
 
@@ -56,3 +61,27 @@ export const userDeleted = functions.auth.user().onDelete((user) => {
 
   return Promise.resolve();
 });
+
+const USERS_COLLECTION = "users";
+
+/**
+ * Called when a document is created in the "users" collection
+ */
+export const onUserDocumentCreated = functions.firestore
+  .document(USERS_COLLECTION + "/{userId}")
+  .onCreate((snapshot, context) => {
+    functions.logger.info("User document created", {
+      uid: context.params.userId,
+    });
+    const data = snapshot.data();
+
+    getSegment().identify({
+      userId: context.params.userId,
+      traits: {
+        githubLogin: data.githubLogin,
+        oshEmail: data.email,
+      },
+    });
+
+    return Promise.resolve();
+  });
